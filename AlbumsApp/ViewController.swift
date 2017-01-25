870946312f8bf83c48a763adb919844bc7174d73
Engylizium <peresves@ya.ru>
2017-01-26 02:34:12 +0700

    Created Title Menu interface and connection to localLib

--- AlbumsApp/ViewController.swift
+++ AlbumsApp/ViewController.swift
@@ -12,7 +12,15 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Theme Setup
+        menuStack.spacing = view.frame.height * 0.3
+        searchButton.layer.borderColor = themeColor.cgColor
+        searchButton.layer.borderWidth = 1
+        searchButton.layer.cornerRadius = 10
+        localAlbumsButton.layer.borderColor = themeColor.cgColor
+        localAlbumsButton.layer.borderWidth = 1
+        localAlbumsButton.layer.cornerRadius = 10
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +28,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBOutlet weak var menuStack: UIStackView!
+    @IBOutlet weak var searchButton: UIButton!
+    @IBOutlet weak var localAlbumsButton: UIButton!
+    
+    let themeColor = #colorLiteral(red: 0.9996318221, green: 0.9908382297, blue: 0.6350821257, alpha: 1)
+    
+    @IBAction func localAlbums(_ sender: UIButton) {
+        callLocalLib()
+    }
+
+    func callLocalLib(){
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let controller = storyboard.instantiateViewController(withIdentifier: "savedList")
+        controller.modalTransitionStyle = .crossDissolve
+        self.present(controller, animated: true, completion: nil)
+    }
 
 }
 
